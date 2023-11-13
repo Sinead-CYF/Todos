4f907c6d6ede88d50410476a6de31fba588292f6
Sinead <77743497+Sinead-CYF@users.noreply.github.com>
2023-11-13 23:57:25 +0000

    created NewForm Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
 import { useState } from "react";
 import "./styles.css";
+import { NewTodoForm} from "./NewTodoForm"; 
 
 export default function App() {
-  const [newItem, setNewItem] = useState("");
+
   const [todos, setTodos] = useState([]);
-
-  function handleSubmit(e) {
-    e.preventDefault();
-
-    setTodos((currentTodos) => {
-      return [
-        ...currentTodos,
-        { id: crypto.randomUUID(), title: newItem, completed: false },
-      ];
-    });
-
-    setNewItem("");
-  }
 
 
   function toggleTodo(id, completed) {
@@ -41,19 +29,7 @@ export default function App() {
 
   return (
     <>
-      <form onSubmit={handleSubmit} className="new-item-form">
-        <div className="form-row">
-          <label htmlFor="new-item">New Item</label>
-          <input
-            value={newItem}
-            onChange={(e) => setNewItem(e.target.value)}
-            type="text"
-            id="item"
-          />
-        </div>
-        <button className="btn">Add</button>
-      </form>
-
+     < NewTodoForm /> 
       <h1 className="header">To Do List</h1>
       <ul className="list">
         {todos.length === 0 && "No To dos, yipee!! "}
@@ -77,3 +53,5 @@ export default function App() {
     </>
   );
 }
+
+
